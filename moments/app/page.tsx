3e3d34a4a08d839b341e6834e55ec62f54239aa8
joho <73869409+johostudio@users.noreply.github.com>
2026-05-23 10:51:04 -0700

    Adjust left icon offset to 0.03rem

--- moments/app/page.tsx
+++ moments/app/page.tsx
@@ -330,7 +330,7 @@ function HeroQuickLinks({ mobile }: { mobile: boolean }) {
           color: "#fff",
           opacity: 0.96,
           position: "relative",
-          marginLeft: "0.08rem",
+          marginLeft: "0.03rem",
           overflow: "visible",
         }}
       >
