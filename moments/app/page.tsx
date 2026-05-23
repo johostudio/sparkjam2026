55c7c5c10370ddfdb8a7344921f4bcaf6f61c6d9
joho <73869409+johostudio@users.noreply.github.com>
2026-05-23 11:36:42 -0700

    Set left icon shift to 1.4rem

--- moments/app/page.tsx
+++ moments/app/page.tsx
@@ -331,7 +331,7 @@ function HeroQuickLinks({ mobile }: { mobile: boolean }) {
           opacity: 0.96,
           position: "relative",
           marginLeft: "0rem",
-          transform: "translateX(2.0rem)",
+          transform: "translateX(1.4rem)",
           overflow: "visible",
         }}
       >
